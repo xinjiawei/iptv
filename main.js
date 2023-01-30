8264e484b1cfb37bbe51e763533b5be827aa2ca8
XINJIAWEI <35734909+xinjiawei@users.noreply.github.com>
2023-01-30 14:39:31 +0800

    Update main.js

--- main.js
+++ main.js
@@ -208,7 +208,7 @@ async function getepg() {
 			  method: 'get',
 			  url: url1,
 			  headers: {
-			    'Accept-Language': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+			    'Accept-Language': 'Mozilla/5.0 Chrome/109.0.0.0 Iptv_simulate'
 			  }
 			};
 			
