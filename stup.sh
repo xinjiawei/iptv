384edbdbb183297e5a8d6b44dab73480f2fb80f7
xinjiawei <xinjiawei@outlook.com>
2025-01-12 12:53:32 +0800

    打包镜像

--- stup.sh
+++ stup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cron
 service lighttpd start
-/app/udpxy -S -a 0.0.0.0 -p 4022 -m 0.0.0.0 -c 5 -l /app/config/udpxy.log -B 2097152 -R 10 -M 300
+/app/udpxy -S -a 0.0.0.0 -p 4022 -m 0.0.0.0 -c 100 -l /app/config/udpxy.log -B 2097152 -R -1 -M 300
 
 # ./heiptv_emulate
-# config auto gen in /app/config
 tail -f /dev/null
