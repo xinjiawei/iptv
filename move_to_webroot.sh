e110daa685add37a1c463ab4fc85cf62d9e0cca3
xinjiawei <xinjiawei@outlook.com>
2025-01-12 11:14:41 +0800

    打包镜像

--- move_to_webroot.sh
+++ move_to_webroot.sh
@@ -1,3 +1,4 @@
 #/bin/bash
+# copy output to web dir
 /bin/cp -rf /app/output/m3u/* /app/output/lighttps/root
 /bin/cp -rf /app/output/epg/* /app/output/lighttps/root
--- stup.sh
+++ stup.sh
@@ -4,4 +4,5 @@ service lighttpd start
 /app/udpxy -S -a 0.0.0.0 -p 4022 -m 0.0.0.0 -c 5 -l /app/config/udpxy.log -B 2097152 -R 10 -M 300
 
 # ./heiptv_emulate
+# config auto gen in /app/config
 tail -f /dev/null
